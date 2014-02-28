911f4240ad76a8aa5e68ea8b9a34f39b637987ad
forcez <info@vindkmo.be>
2014-02-28 15:55:33 +0100

    demo_v2

--- commons.js
+++ commons.js
@@ -1,25 +1,19 @@
 // Global variable that will tell us whether PhoneGap is ready
 var isPhoneGapReady = false;
 function init() {
- // Add an event listener for deviceready
- document.addEventListener("deviceready",
- onDeviceReady, false);
- // Older versions of Blackberry < 5.0 don't support
- // PhoneGap's custom events, so instead we need to perform
- // an interval check every 500 milliseconds to see whether
- // PhoneGap is ready. Once done, the interval will be
- // cleared and normal processing can begin.
- var intervalID = window.setInterval(function() {
- if (PhoneGap.available) {
- onDeviceReady();
- }
- }, 500);
+	// Add an event listener for deviceready
+	document.addEventListener("deviceready",onDeviceReady, false);
+
+	var intervalID = window.setInterval(function() {
+		if (PhoneGap.available) {
+			onDeviceReady();
+		}
+	}, 500);
 }
-function onDeviceReady() {
- window.clearInterval(intervalID);
- // set to true
- isPhoneGapReady = true;
- alert('The device is now ready');
-}
+	function onDeviceReady() {
+		window.clearInterval(intervalID);
+		isPhoneGapReady = true;
+		alert('The device is now ready');
+	}
 // Set an onload handler to call the init function
 window.onload = init;
